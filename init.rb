80d16d6b49237f080237c896ede6ab932d64cb62
smalex <ap.smir@gmail.com>
2022-09-02 00:39:28 +0300

    change version

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ require_dependency 'redmine_post_action_sql'
 Redmine::Plugin.register :redmine_post_action_sql do
   name 'Redmine Post-Action SQL'
   author 'Alexey Smirnov'
-  url ''
+  url 'https://github.com/apsmir/redmine_post_action_sql'
   description 'Execute SQL expression when post action'
-  version '1.0.0'
+  version '1.1'
 end
