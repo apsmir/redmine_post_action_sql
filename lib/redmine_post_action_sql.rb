60f40bf76a15e92aa8270466f5561b44cbae779f
smalex <ap.smir@gmail.com>
2022-09-02 00:36:07 +0300

    add controller_issues_bulk_edit_before_save

--- lib/redmine_post_action_sql.rb
+++ lib/redmine_post_action_sql.rb
@@ -7,6 +7,10 @@ class RedminePostActionHooks < Redmine::Hook::Listener
     execute_sql(context)
   end
 
+  def controller_issues_bulk_edit_before_save(context = { })
+    execute_sql(context)
+  end
+
   def execute_sql(context)
     id = context[:issue].id.to_s
     s = __FILE__.gsub('lib/redmine_post_action_sql.rb','sql/*.sql')
